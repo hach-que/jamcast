5c5a6165d8b2c8b75a7a6b98645cac8a071a1a41
June Rhodes <jrhodes@redpointgames.com.au>
2011-01-05 10:36:28 +1100

    Added initial codebase.

---
+++ Server/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace JamCast
+{
+    public abstract class Client
+    {
+        /// <summary>
+        /// Retrieves a bitmap of the client's screen.
+        /// </summary>
+        /// <returns>A bitmap of the client's screen.</returns>
+        public abstract Bitmap GetScreen();        
+    }
+}
